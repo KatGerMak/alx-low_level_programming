e819358bdab6574f6a36956a3c0403e821f223ad
Katleho Makumane <kmakumane95@gmail.com>
2023-08-15 16:29:52 -0700

    t 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * @c: char to check
  * Return: 1(lowercase) 0(not lowercase)
  */
-int _islower(char c)
+int _islower(int c)
 {
 	if( c >= 'a' && c <= 'z')
 	{
---
+++ 0x02-functions_nested_loops/is-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check if alphabet
+ * @c: char to check
+ * Return: 1(is alphabet) 0(not alphabet)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (x >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
