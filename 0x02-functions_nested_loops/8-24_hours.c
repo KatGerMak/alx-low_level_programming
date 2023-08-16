9ba1cda517ea69ab7ff26935b828285244437b1c
Katleho Makumane <kmakumane95@gmail.com>
2023-08-15 18:22:16 -0700

    l

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
 	int i;
 	int n;
 
-	for (i = 0; i <24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		for (n = 0; n < 60; n++)
 		{
