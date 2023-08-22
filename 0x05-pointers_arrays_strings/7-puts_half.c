7b515ddfe9ebbdc0df3f87ebfe0a5c6347f04ad0
Katleho Makumane <kmakumane95@gmail.com>
2023-08-21 20:23:24 -0700

    6 - 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+
+	for (i = 0; str[i] != '\0'; a++)
+	{
+		len++;
+	}
+
+	n = len / 2;
+
+	if ((len % 2) == 1)
+	{
+		n = ((len + 1) / 2);
+	}
+
+	for (i = n; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-prints_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_array - prints array elements
+ * @a: pointer to array
+ * @n: number elemnts in array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d", a[i]);
+
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+	}
+
+	prinf("\n");
+}
