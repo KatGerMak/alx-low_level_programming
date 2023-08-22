ba9ef65889fbe847c3f7a03303937edd6b8cdac9
Katleho Makumane <kmakumane95@gmail.com>
2023-08-21 20:46:28 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strcpy - copy string from pointer
+ * @dest: copy to
+ * @src: copy from
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int x;
+
+	while (*(src + i) != '\0')
+		i++;
+
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
