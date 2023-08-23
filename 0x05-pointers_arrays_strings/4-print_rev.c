23f6ff53db59e6897f46742e4ca2c2417ab3269c
Katleho Makumane <kmakumane95@gmail.com>
2023-08-22 17:19:32 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,21 @@
 #include "main.h"
 /**
  * print_rev - print string in reverse
- * @*s: pointer to string to print
+ * @s: pointer to string to print
  */
 void print_rev(char *s)
 {
 	int len = 0;
 	int i;
 
-	while (*s != '\0')
+	while (s[len] != '\0')
 	{
 		len++;
-		s++;
 	}
-	s--;
 
 	for (i = len - 1; 1 >= 0; i--)
 	{
-		_putchar(*s);
-		s--;
+		_putchar(s[i]);
 	}
 
 	_putchar('\n');
