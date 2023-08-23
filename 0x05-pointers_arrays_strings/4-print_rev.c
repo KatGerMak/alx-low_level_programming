1b22dc180f2c0689c6274b5999bdad825e85c69a
Katleho Makumane <kmakumane95@gmail.com>
2023-08-22 17:28:12 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,6 +5,17 @@
  */
 void print_rev(char *s)
 {
+	int i;
+
+	i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	/*
 	int len = 0;
 	int i;
 
@@ -16,7 +27,7 @@ void print_rev(char *s)
 	for (i = len - 1; 1 >= 0; i--)
 	{
 		_putchar(s[i]);
-	}
+	}*/
 
 	_putchar('\n');
 }
