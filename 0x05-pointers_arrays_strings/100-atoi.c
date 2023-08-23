07f4bf31c67eb0eb7fc32dce6dfb27024d5aaedb
Katleho Makumane <kmakumane95@gmail.com>
2023-08-22 19:11:05 -0700

    10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,10 +16,10 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result 8 10 + (s[i] - '0');
+			result = result * 10 + (s[i] - '0');
 
 			if (( sign == 1 && result < 0) || ( sign == -1 && result > 0))
-				returb (0);
+				return (0);
 		}
 		else if ( result != 0)
 			break;
