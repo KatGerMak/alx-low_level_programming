5a8aab0631ff5b071012d795c9ce34ea43dc44d5
Katleho Makumane <kmakumane95@gmail.com>
2023-08-24 18:58:49 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - convert lowercase to uppercase
+ * @str: string to change
+ * Return: changed string pointer
+ */
+char *string_toupper(char *str)
+{
+	char *p = str;
+
+	while(*p != '\0')
+	{
+		if (*p >= 'a' && *p <= 'z')
+		{
+			*p = *p - 'a' + 'A';
+		}
+
+		p++
+	}
+
+	return (str);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * cap_string - capitallize words
+ * @*: string to capitalize
+ * Return: result string
+ */
+char *cap_string(char *)
+{
+
