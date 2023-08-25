5bc03cf7056419880bb24aa7d76efa18314bbcf6
Katleho Makumane <kmakumane95@gmail.com>
2023-08-24 19:13:48 -0700

    6

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,7 +8,7 @@ char *string_toupper(char *str)
 {
 	char *p = str;
 
-	while(*p != '\0')
+	while (*p != '\0')
 	{
 		if (*p >= 'a' && *p <= 'z')
 		{
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,22 @@
 #include "main.h"
 /**
  * cap_string - capitallize words
- * @*: string to capitalize
+ * @str: string to capitalize
  * Return: result string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	int i = 0;
 
+	while (str[i])
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			str[i] = toupper(str[i]);
+		}
+
+		i++;
+	}
+
+	return (str);
+}
