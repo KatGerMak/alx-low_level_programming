f1f2023269ead0d0c23edb02e19f8ff9d920c261
Katleho Makumane <kmakumane95@gmail.com>
2023-08-30 18:23:25 -0700

    1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts using resursion
+ * @s: string to put
+ */
+void _pputs_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursions(s + 1);
+	}
+	else
+	{
+		_putchar('\n'):
+	}
+}
