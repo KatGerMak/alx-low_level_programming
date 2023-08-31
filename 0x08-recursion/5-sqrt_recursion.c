4ba7d8605c7a6d3eeddd0a06291385103f702d2f
Katleho Makumane <kmakumane95@gmail.com>
2023-08-30 19:48:13 -0700

    6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,30 @@
 #include "main.h"
 /**
+ * sqrt_rec - calc square root recursice
+ * @n: number to check
+ * @i: int guess
+ * Return: the square root, -1 not found
+ */
+int sqrt_rec(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+
+	if (i * i > n)
+		return (-1);
+
+	return (sqrt_rec(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - get square root of a number
  * @n: number to get square root from
  * Return: square root
  */
 int _sqrt_recursion(int n)
-{}
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqrt_rec(n, 0));
+}
---
+++ 0x08-recursion/6-prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+int is_prime_rec(int n, int divi);
+
+/**
+ * is_prime_rec - check prime recurssively
+ * @n: num to check
+ * @divi: divisor to check
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_rec(int n, int divi)
+{
+	if (n < 2)
+		return (0);
+
+	if (divi * divi > n)
+		return (1);
+
+	if (n % divi == 0)
+		return 0;
+
+	return (is_prime_rec(n, divi + 1));
+}
+
+/**
+ * is_prime_number - check if prime nuber
+ * @n: int tocheck
+ * Return: 1 if prime, 0 not prime
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_rec(n, 2));
+}
