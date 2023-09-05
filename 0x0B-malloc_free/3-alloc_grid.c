7e80802fc8de4006ee4ea0daa60d680cf67204a0
Katleho Makumane <kmakumane95@gmail.com>
2023-09-05 07:13:36 -0700

    0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,12 +27,12 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = 0; j < i; j++)
 				free(grid[j]);
-			
+
 			free(grid);
 			return (NULL);
 		}
-		
-		for (j = 0; j <width; j++)
+
+		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
 	return (grid);
