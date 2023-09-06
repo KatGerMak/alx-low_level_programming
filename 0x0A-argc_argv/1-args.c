b815d89ce2a65813af4cddfcfe4681bac78e4539
Katleho Makumane <kmakumane95@gmail.com>
2023-09-06 12:39:58 -0700

    0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print args
+ * @argc: size array
+ * @argv: array of argss
+ * Return: Always 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
