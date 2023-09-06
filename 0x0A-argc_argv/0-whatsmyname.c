83ac76ece48e57c7712d255da328582d3814b4f2
Katleho Makumane <kmakumane95@gmail.com>
2023-09-06 06:23:33 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: array size
+ * @argv: array
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf ("\%s\n", argv[0]);
+		return (0);
+	}
+
+	return (1);
+}
